53a8ff071ef2b7d180b486dfc9ef389e9e70f1f6
Ostroukh <nikolai.ostroukh@viasat.com>
2024-04-18 10:36:46 +0200

    added comments before each function

--- contracts/sendMoney.sol
+++ contracts/sendMoney.sol
@@ -7,13 +7,16 @@ contract SendWithdrawMoney {
     function balanceDeposit() public payable {
         balanceReceived += msg.value;
     }
+    //function describing getting the contract balance
     function getContractBalance() public view returns(uint) {
         return address(this).balance;
     }
+    ////function for withdraw to sender
     function withdrawAllTo() public {
         address payable to = payable(msg.sender);
         to.transfer(getContractBalance());
     }
+    ////function for withdraw to any account
     function withdrawTo(address payable to) public {
         to.transfer(getContractBalance());
     }
